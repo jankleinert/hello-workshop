10768786384bd645e03aa711f76ce557b4338533
Jan Kleinert <jan.kleinert@redhat.com>
2018-12-06 13:35:13 -0500

    Update server.js message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Node+JS Interactive!\n');
+  res.end('Hello there!\n');
 });
 
 server.listen(port, hostname, () => {
